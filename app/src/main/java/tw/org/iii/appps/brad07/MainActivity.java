91969c7482a14d134445ac3e544a894ade09f8b7
Brad <brad@brad.tw>
2019-07-28 09:35:33 +0800

    02

--- app/src/main/java/tw/org/iii/appps/brad07/MainActivity.java
+++ app/src/main/java/tw/org/iii/appps/brad07/MainActivity.java
@@ -1,10 +1,14 @@
 package tw.org.iii.appps.brad07;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -12,11 +16,16 @@ import java.util.TimerTask;
 public class MainActivity extends AppCompatActivity {
     private Timer timer;
     private int k = 0;
+    private TextView mesg;
+    private UIHandler uiHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        uiHandler = new UIHandler();
+        mesg = findViewById(R.id.mesg);
 
         timer = new Timer();
         timer.schedule(new MyTask(), 0, 1000);
@@ -28,6 +37,13 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 for (int i=0; i<20; i++){
                     Log.v("brad", "i = " + i);
+
+                    Message message = new Message();
+                    Bundle data = new Bundle();
+                    data.putInt("i", i);
+                    message.setData(data);
+                    uiHandler.sendMessage(message);
+
                     try {
                         Thread.sleep(1000);
                     }catch (Exception e){
@@ -37,6 +53,18 @@ public class MainActivity extends AppCompatActivity {
             }
         }.start();
 
+    }
+
+
+    private class UIHandler extends Handler {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+
+            Bundle data = msg.getData();
+            int i = data.getInt("i");
+            mesg.setText("i = " + i);
+        }
     }
 
     private class MyTask extends TimerTask {
